0b83e1056fff999dd58c6ab39740d601df382619
dannyhma <dannyhimawan089@gmail.com>
2024-09-27 13:42:31 +0700

    Layouting With Prime Vue And Tailwind-PrimeUI

---
+++ client/src/components/Layouts/Header.vue
@@ -0,0 +1,55 @@
+<template>
+	<div>
+		<Menubar class="bg-yellow-300" :model="items">
+			<template #end>
+				<Button
+					class="border-yellow-900 bg-yellow-700"
+					label="Login"
+					icon="pi pi-user"
+					@click="dialog = true"
+				/>
+			</template>
+		</Menubar>
+
+		<!-- Dialog -->
+		<FormAuthComponent v-model:visible="dialog" />
+	</div>
+</template>
+
+<script setup>
+import Menubar from "primevue/menubar";
+import { ref } from "vue";
+import FormAuthComponent from "../Fragments/FormAuthComponent.vue";
+
+const items = ref([
+	{
+		label: "Home",
+		icon: "pi pi-home",
+	},
+	{
+		label: "Features",
+		icon: "pi pi-star",
+	},
+	{
+		label: "Projects",
+		icon: "pi pi-search",
+		items: [
+			{
+				label: "Components",
+				icon: "pi pi-bolt",
+			},
+			{
+				label: "Blocks",
+				icon: "pi pi-server",
+			},
+		],
+	},
+	{
+		label: "Contact",
+		icon: "pi pi-envelope",
+	},
+]);
+
+// state
+const dialog = ref(false);
+</script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,33 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// styling
+import "./style.css";
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
+import "primeicons/primeicons.css";
+
+// element
+import Dialog from "primevue/dialog";
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
+
+const app = createApp(App);
+
+// app.use(router);
+app.component("Dialog", Dialog);
+app.component("Button", Button);
+app.component("InputText", InputText);
+app.use(PrimeVue, {
+	theme: {
+		preset: Aura,
+		options: {
+			cssLayer: {
+				name: "primevue",
+				order: "tailwind-base, primevue, tailwind-utilities",
+			},
+		},
+	},
+});
+
+app.mount("#app");
