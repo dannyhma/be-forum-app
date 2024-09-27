a14f37be97eb074482ce7afef0843a131c9ae67d
dannyhma <dannyhimawan089@gmail.com>
2024-09-27 23:17:21 +0700

    Secure Route Vue with Route Guard

---
+++ client/src/components/Elements/AlertAuth.vue
@@ -0,0 +1,17 @@
+<template>
+	<Toast severity="danger" v-show="showError"></Toast>
+</template>
+
+<script setup>
+import { useToast } from "primevue/usetoast";
+const toast = useToast();
+
+const showError = () => {
+	toast.add({
+		severity: "error",
+		summary: "Error Message",
+		detail: "Message Content",
+		life: 3000,
+	});
+};
+</script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../pages/HomeView.vue";
 import DahsboardView from "../pages/DashboardView.vue";
+import { useAuthStore } from "../stores/authStores";
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,8 +20,21 @@ const router = createRouter({
 			path: "/dashboard",
 			name: "dashboard",
 			component: DahsboardView,
+			meta: {
+				requiredAuth: true,
+			},
 		},
 	],
+});
+
+router.beforeEach(async (to, from) => {
+	const authStores = await useAuthStore();
+	if (to.meta.requiredAuth && !authStores.currentUser) {
+		alert("You must be logged in to access this page");
+		return {
+			path: "/",
+		};
+	}
 });
 
 export default router;
